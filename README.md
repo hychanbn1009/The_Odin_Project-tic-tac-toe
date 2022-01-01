7b8a0a5b62692ed867a435ce961ee1ab92bd0d9f
fishball <hychanbn1009@gmail.com>
2022-01-01 20:59:55 +0800

    basic function

--- README.md
+++ README.md
@@ -13,10 +13,18 @@
 - [x] Create Game Board
 - [X] Allow Player add Mark in Game Board by onClick function
 - [X] Disable Player re-click the selected grid
-- [ ] Check it is player turn or computer turn
+- [X] Check it is player turn or computer turn
 - [X] If computer turn, computer add mark in Game Board
 - [X] Store All the mark/selection into array
 - [X] Computer & Player Selection should update the array
-- [ ] Check if computer & player win in their round. if no, change to next turn. If no one win and all grid are filled. This round draw.
+- [X] Check if computer & player win in their round. if no, change to next turn. If no one win and all grid are filled. This round draw.
+- [ ] A better style
+- [ ] Maybe a smarter computer player
 
 ## Difficulties
+1. Stuck when computer complete their move and change to player turn
+    - Redesign the logically workflow by writing on paper
+2. Tried to use count to control computer and player turn. But the count seems not update correctly
+    - Using the remaining position length to determine. odd number will be player and even number will be computer
+
+    
--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -5,59 +5,129 @@ const Gameboard =()=>{
 
     const [judge,setJudge]=useState(['','','','','','','','',''])
     const [win,setWin]=useState(false)
+    const [count,setCount]=useState(1)
     const [availablePosition,setAvailablePosition]=useState([0,1,2,3,4,5,6,7,8])
 
-    const selection=(index,player_selection)=>{
-        const copy_judge=judge
-        const copy_availablePosition=availablePosition
-        copy_judge[index]=player_selection
-        copy_availablePosition.splice(copy_availablePosition.indexOf(index),1)
-        setJudge(copy_judge)
-        setAvailablePosition(copy_availablePosition)
-        checkWinner()
-        {console.log(win)}
+    const handleClick=(event)=>{
+        if(win===false&&count%2===1){
+            document.getElementById(event.target.id).innerHTML='X'
+            document.getElementById(event.target.id).disabled=true
+            const copy_judge=judge
+            const copy_availablePosition=availablePosition
+            copy_judge[event.target.id]='X'
+            copy_availablePosition.splice(copy_availablePosition.indexOf(parseInt(event.target.id)),1)
+            setJudge(copy_judge)
+            setAvailablePosition(copy_availablePosition)
+            // selection(parseInt(event.target.id),'X')
+            setCount(count=>count+1)
+            {console.log(availablePosition,availablePosition.length)}
+            checkWinner()
+        }
     }
 
-    const handleClick=(event)=>{
-        document.getElementById(event.target.id).innerHTML='X'
-        document.getElementById(event.target.id).disabled=true
-        selection(parseInt(event.target.id),'X')
-        if(win===false){
+    const checkWinner=()=>{
+        {console.log(judge,count)}
+        if (judge[0]===judge[1]&&judge[1]===judge[2]&&judge[0]!==''){
+            if(judge[0]==='X'){
+                setWin('Player Win')
+            }else if(judge[0]==='O'){
+                setWin('Computer Win')
+            }
+        }
+        else if (judge[3]===judge[4]&&judge[4]===judge[5]&&judge[3]!==''){
+            if(judge[3]==='X'){
+                setWin('Player Win')
+            }else if(judge[3]==='O'){
+                setWin('Computer Win')
+            }
+        }
+        else if (judge[6]===judge[7]&&judge[7]===judge[8]&&judge[6]!==''){
+            if(judge[6]==='X'){
+                setWin('Player Win')
+            }else if(judge[6]==='O'){
+                setWin('Computer Win')
+            }
+        }
+        else if (judge[0]===judge[4]&&judge[4]===judge[8]&&judge[0]!==''){
+            if(judge[0]==='X'){
+                setWin('Player Win')
+            }else if(judge[0]==='O'){
+                setWin('Computer Win')
+            }
+        }
+        else if (judge[2]===judge[4]&&judge[4]===judge[6]&&judge[2]!==''){
+            if(judge[2]==='X'){
+                setWin('Player Win')
+            }else if(judge[2]==='O'){
+                setWin('Computer Win')
+            }
+        }
+        else if (judge[0]===judge[3]&&judge[3]===judge[6]&&judge[0]!==''){
+            if(judge[0]==='X'){
+                setWin('Player Win')
+            }else if(judge[0]==='O'){
+                setWin('Computer Win')
+            }
+        }
+        else if (judge[1]===judge[4]&&judge[4]===judge[7]&&judge[1]!==''){
+            if(judge[1]==='X'){
+                setWin('Player Win')
+            }else if(judge[1]==='O'){
+                setWin('Computer Win')
+            }
+        }
+        else if (judge[2]===judge[5]&&judge[5]===judge[8]&&judge[2]!==''){
+            if(judge[2]==='X'){
+                setWin('Player Win')
+            }else if(judge[2]==='O'){
+                setWin('Computer Win')
+            }
+        }
+        else if (availablePosition.length===0){
+            setWin('Draw')
+        }
+        else if(availablePosition.length%2===0){
             computerPlayer()
-        }else{
-            {console.log("Finished!")}
         }
     }
 
     const computerPlayer=()=>{
-        if(availablePosition.length!==0 && win===false){
-            {console.log("Computer!")}
-            const random_number=Math.floor(Math.random() * availablePosition.length)
-            const selected_position=availablePosition[random_number]
-            document.getElementById(selected_position).innerHTML='O'
-            selection(selected_position,'O')
-            document.getElementById(selected_position).disabled=true
-        }
-    }
-    
-    const checkWinner=()=>{
-        if (judge[3]===judge[4]&&judge[4]===judge[5]&&judge[3]!==''){
-            setWin(win=>!win)
-            {console.log("Win!")}
+        if(win===false&&availablePosition!==0){
+            setTimeout(()=>{
+                const random_number=Math.floor(Math.random() * availablePosition.length)
+                const selected_position=availablePosition[random_number]
+                document.getElementById(selected_position).innerHTML='O'
+                document.getElementById(selected_position).disabled=true
+                const copy_judge=judge
+                const copy_availablePosition=availablePosition
+                copy_judge[selected_position]='O'
+                copy_availablePosition.splice(copy_availablePosition.indexOf(parseInt(selected_position)),1)
+                setJudge(copy_judge)
+                setAvailablePosition(copy_availablePosition)
+                setCount(count=>count+1)
+                {console.log('computer',availablePosition,availablePosition.length)}
+                checkWinner()
+            },1000)
+        }else{
+            setWin('Draw')
         }
     }
 
     return(
+        <div className='display'>
+        <div>Round:{count}</div>
+        {win?(<span>{win}</span>):''}
         <div className='gameboard'>
-            <button id='0' className='top-row left-column' onClick={(event)=>{handleClick(event)}}></button>
-            <button id='1' className='top-row middle-column' onClick={(event)=>{handleClick(event)}}></button>
-            <button id='2' className='top-row right-column' onClick={(event)=>{handleClick(event)}}></button>
-            <button id='3' className='middle-row left-column' onClick={(event)=>{handleClick(event)}}></button>
-            <button id='4' className='middle-row middle-column' onClick={(event)=>{handleClick(event)}}></button>
-            <button id='5' className='middle-row right-column' onClick={(event)=>{handleClick(event)}}></button>
-            <button id='6' className='bottom-row left-column' onClick={(event)=>{handleClick(event)}}></button>
-            <button id='7' className='bottom-row middle-column' onClick={(event)=>{handleClick(event)}}></button>
-            <button id='8' className='bottom-row right-column' onClick={(event)=>{handleClick(event)}}></button>
+            <button id='0' className='btn top-row left-column' onClick={(event)=>{handleClick(event)}}></button>
+            <button id='1' className='btn top-row middle-column' onClick={(event)=>{handleClick(event)}}></button>
+            <button id='2' className='btn top-row right-column' onClick={(event)=>{handleClick(event)}}></button>
+            <button id='3' className='btn middle-row left-column' onClick={(event)=>{handleClick(event)}}></button>
+            <button id='4' className='btn middle-row middle-column' onClick={(event)=>{handleClick(event)}}></button>
+            <button id='5' className='btn middle-row right-column' onClick={(event)=>{handleClick(event)}}></button>
+            <button id='6' className='btn bottom-row left-column' onClick={(event)=>{handleClick(event)}}></button>
+            <button id='7' className='btn bottom-row middle-column' onClick={(event)=>{handleClick(event)}}></button>
+            <button id='8' className='btn bottom-row right-column' onClick={(event)=>{handleClick(event)}}></button>
+        </div>
         </div>
     )
 }
